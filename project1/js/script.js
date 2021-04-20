b655036d1e7b7151b2f78afcc36ad1a3fe139b44
seoyeonpp <seoyeonpub@gmail.com>
2021-04-20 13:25:31 +0900

    top버튼 및 favicon추가

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // 탭메뉴
     $('ul.tab_menu_tit li').click(function () {
         var on = $(this).attr('data-tab');
 
@@ -9,4 +10,28 @@ $(document).ready(function () {
         $("#" + on).addClass('on');
         return false;
     })
+    // 상단메뉴
+    $('.rollover').each(function () {
+        var a = $(this);
+        a.hover(function () {
+            a.css('color', '#005fca')
+        }, function () {
+            a.css('color', '#000000')
+        })
+    })
 });
+
+let Top = document.querySelector('#top_btn');
+
+window.addEventListener('scroll', function () {
+    if (this.scrollY > 400) {
+        Top.classList.add('on');
+    } else {
+        Top.classList.remove('on');
+    }
+})
+
+Top.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
