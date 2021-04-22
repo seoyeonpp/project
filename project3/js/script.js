e96d08d1557cdb4f11d81576eb894b9eade29ed0
seoyeonpp <seoyeonpub@gmail.com>
2021-04-22 23:34:29 +0900

    bxslider수정

---
+++ project3/js/script.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.main_slide>ul').bxSlider({
+        auto: true,
+        stopAutoOnClick: true,
+        speed: 1500,
+        pause: 3000,
+        autoHover: true,
+        infiniteLoop: true,
+        pager: true,
+        controls: false,
+    })
+})
