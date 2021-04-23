a85d23fd4349f0b4572301f8ac853e12685b3a19
seoyeonpp <seoyeonpub@gmail.com>
2021-04-23 18:45:22 +0900

    section슬라이더추가

--- project1/js/script.js
+++ project1/js/script.js
@@ -21,6 +21,68 @@ $(document).ready(function () {
     })
 });
 
+// #contents_bottom 슬라이드
+const slides = document.querySelector('#contents_bottom .slides'),
+    slide = document.querySelectorAll('.slides li'),
+    slideCount = slide.length,
+    prevBtn = document.querySelector('.control_box>.slide_prev'),
+    nextBtn = document.querySelector('.control_box>.slide_next'),
+    slideWidth = 260
+let currentIdx = 0;
+slides.style.width = (slideWidth * slideCount) + 'px';
+
+function moveSlide(num) {
+    slides.style.left = -num * slideWidth + 'px';
+    currentIdx = num;
+};
+nextBtn.addEventListener('click', function () {
+    if (currentIdx < slideCount - 5) {
+        moveSlide(currentIdx + 1);
+    } else {
+        moveSlide(0);
+    }
+});
+prevBtn.addEventListener('click', function () {
+    if (currentIdx > 0) {
+        moveSlide(currentIdx - 1);
+    } else {
+        moveSlide(slideCount - 5);
+    }
+});
+
+// #article3 뉴스슬라이더
+const newsSlider = document.querySelector('.wrapper .newsletter'),
+    slideLi = document.querySelectorAll('.newsletter>li'),
+    newsCount = slideLi.length,
+    newsprev = document.querySelector('.news_control>.news_control_prev'),
+    newsnext = document.querySelector('.news_control>.news_control_next'),
+    sWidth = 210,
+    sPadding = 40
+let currentnews = 0;
+
+function newsMove(list) {
+    newsSlider.style.left = -list * (sWidth + sPadding) + 'px';
+    currentnews = list;
+}
+
+newsnext.addEventListener('click', function () {
+    if (currentnews < newsCount - 4) {
+        newsMove(currentnews + 1);
+    } else {
+        newsMove(0);
+    }
+})
+
+newsprev.addEventListener('click', function () {
+    if (currentnews > 0) {
+        newsMove(currentnews - 1);
+    } else {
+        newsMove(newsCount - 4);
+    }
+})
+
+
+
 // 푸터 관련사이트
 const siteBtn = document.querySelector('.site button');
 const siteList = document.querySelector('.site .site_select');
